222c2679642a1554c1910c3a0c749ad6321c1fc8
Guntrolu Pavan Prasanth <pavanprasanth48850@gmail.com>
2024-06-30 13:52:11 +0530

    Displaying a Loading Indicator

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
-import { Outlet } from "react-router-dom";
+import Loader from "./loader";
+import { Outlet, useNavigation } from "react-router-dom";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
 
       <main>
---
+++ src/ui/loader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function loader() {
+  return <div className="loader"></div>;
+}
+
+export default loader;
